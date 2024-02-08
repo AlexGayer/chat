35101d59dad6f14182a5dc5afd0ecea5e2937889
Alex Gayer <alexandregs119@hotmail.com>
2024-02-08 09:53:58 -0300

    create app chat

---
+++ lib/core/models/chat_notification.dart
@@ -0,0 +1,9 @@
+class ChatNofification {
+  final String title;
+  final String body;
+
+  const ChatNofification({
+    required this.body,
+    required this.title,
+  });
+}
---
+++ lib/core/services/notificiation/chat_notification_service.dart
@@ -0,0 +1,24 @@
+import 'package:chat/core/models/chat_notification.dart';
+import 'package:flutter/material.dart';
+
+class ChatNotificationService with ChangeNotifier {
+  List<ChatNofification> _items = [];
+
+  int get itemsCount {
+    return _items.length;
+  }
+
+  List<ChatNofification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNofification nofification) {
+    _items.add(nofification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+    notifyListeners();
+  }
+}
